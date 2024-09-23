b914fc51b28e898531361d1bc56d1cdcadf43bd4
Felipe Santiago Morais <contatofelipesantiago@gmail.com>
2024-09-23 16:34:56 -0300

    configured vitest and react-testing-library

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    setupFiles: "./setupFiles.ts",
+    environment: "happy-dom"
+  }
 });
