0aa1e0d30f7c66f336a6071e2adc273d9981a3d6
Felipe Santiago Morais <contatofelipesantiago@gmail.com>
2024-09-24 01:16:39 -0300

    fixing footer on the bottom of the page

--- src/components/portfolio/index.tsx
+++ src/components/portfolio/index.tsx
@@ -2,9 +2,8 @@ import { Hero } from "./hero"
 
 export const Home = () => {
     return (
-        <>
+        <div className="flex-1 flex items-center">
         <Hero />
-        
-        </>
+        </div>
     )
 }
