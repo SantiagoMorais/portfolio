2fab2a2bab630966da09b3ed4918705c865509b6
Felipe Santiago Morais <contatofelipesantiago@gmail.com>
2024-09-24 05:54:05 -0300

    created themeToggler context

---
+++ src/contexts/themeToggler.tsx
@@ -0,0 +1,24 @@
+import { createContext, useState, useContext, ReactNode } from "react";
+
+const ThemeContext = createContext({
+  theme: "light",
+  toggleTheme: () => {}
+});
+
+export const useTheme = () => useContext(ThemeContext);
+
+export const ThemeProvider = ({ children }: { children: ReactNode }) => {
+  const [theme, setTheme] = useState("dark");
+
+  const toggleTheme = () => {
+    const newTheme = theme === "dark" ? "light" : "dark";
+    setTheme(newTheme);
+    document.body.classList.toggle("light", newTheme === "light");
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
