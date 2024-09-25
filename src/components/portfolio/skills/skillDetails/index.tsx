ad2e0725aed2cfb132b816ebeb2a7115b3dfbaf1
Felipe Santiago Morais <contatofelipesantiago@gmail.com>
2024-09-25 07:30:53 -0300

    changed style of SkillsDetails component

--- src/components/portfolio/skills/skillDetails/index.tsx
+++ src/components/portfolio/skills/skillDetails/index.tsx
@@ -9,9 +9,7 @@ type SkillDetailsProps = {
 
 export const SkillDetails = ({ category, icon, list }: SkillDetailsProps) => {
   return (
-    <div
-      className="max-w-[40rem] flex-1 p-4 rounded-md border-2 border-secondary flex flex-col items-center min-w-[20rem]"
-    >
+    <div className="max-w-[40rem] flex-1 px-4 py-8 rounded-xl bg-secondaryPanel flex flex-col items-center min-w-[20rem]">
       <div className="flex items-center gap-4 mb-4 relative">
         <FontAwesomeIcon
           className="text-primary border-2 border-primary mb-4 p-4 size-[2.5rem] rounded-full"
@@ -24,7 +22,7 @@ export const SkillDetails = ({ category, icon, list }: SkillDetailsProps) => {
       <ul className="flex flex-col gap-1 text-small text-center">
         {list.map((item, idx) => (
           <li
-            className="text-textColor mb-1 pb-1 last-of-type:border-none relative after:absolute after:h-px after:w-full after:bottom-0 after:left-0 after:bg-gradient-to-r from-panel via-primary to-panel"
+            className="text-textColor mb-1 pb-1 relative after:absolute after:h-px after:w-full after:bottom-0 after:left-0 after:bg-gradient-to-r from-panel via-primary to-panel last-of-type:after:hidden"
             key={idx}
           >
             {item}
