9e793d91346e815417b995fe544add1020e4e851
Felipe Santiago Morais <contatofelipesantiago@gmail.com>
2024-09-25 07:41:15 -0300

    fixing banners size

--- src/components/portfolio/projects/projectsList/banner/index.tsx
+++ src/components/portfolio/projects/projectsList/banner/index.tsx
@@ -3,11 +3,11 @@ import { ProjectProps } from "utils/types";
 
 export const Banner = ({ project }: ProjectProps) => {
   return (
-    <div className="max-h-[30rem] sm:max-h-[40rem] h-full rounded-lg overflow-hidden">
+    <div className="max-h-[30rem] sm:max-h-[35rem] h-full rounded-lg overflow-hidden">
       <img
         src={project.banner ? project.banner : imageNotFound}
         alt={project.name}
-        className="size-full object-left-top object-cover"
+        className="h-full sm:h-[35rem] w-full object-left-top object-cover"
       />
     </div>
   );
